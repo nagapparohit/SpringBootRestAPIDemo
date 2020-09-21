5a9f03e445919c360f64c6a45f6097539c9accd2
rohit nagappa <rnagappa35@gmail.com>
2020-09-22 01:00:01 +0530

    spingboot annotation desc

--- src/main/java/com/spirngboot/basics/springboot10steps/Springboot10StepsApplication.java
+++ src/main/java/com/spirngboot/basics/springboot10steps/Springboot10StepsApplication.java
@@ -2,7 +2,13 @@ package com.spirngboot.basics.springboot10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author 91783
+ * SpringBootApplication annotation indicates that it is spring context file.
+ * It enable auto config
+ * It enable component scan
+ */
 @SpringBootApplication
 public class Springboot10StepsApplication {
 
